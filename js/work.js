0d83a2f797ccf4bc750e191ae585cf43b4766d83
ShashidharM0118 <shashidhar.i.0119@gmail.com>
2023-08-25 10:14:04 +0530

    score-tracking ,player-first chance and countdown are implemented with sound(still js unorganized)

--- js/work.js
+++ js/work.js
@@ -1,7 +1,7 @@
 const gridContainer = document.querySelector('.grid-container');
 
   // Organize the grid with the numbers
-  const randomNumbersArray = randomNumbersGenerator(9,9);
+  const randomNumbersArray = randomNumbersGenerator(16,16);
   randomNumbersArray.forEach(number => {
     const gridItem = document.createElement('div');
     gridItem.className = 'grid-item';
@@ -23,39 +23,57 @@ const gridContainer = document.querySelector('.grid-container');
     }
     return result;
   }
-
+  var counter = document.getElementById("timer").innerHTML = 0;
+  var scoreCard = document.querySelector(".scoreboard");
+  var  timerNote= document.querySelector(".timer-note");
+  var count = 10;
   //Time delay to show the numbers
   const gridItems = document.querySelectorAll('.grid-item');
+  var timer =  document.getElementById("timer");
+  function isTimer(){return true;};
+  var audio = document.getElementById("audioId");
+  setInterval(() => {
+    if (count >= 0) {
+      timer.innerHTML = count;
+      audio.play();
+      count--;
+    }
+  }, 1000);
+  
   setTimeout(() => {
     gridItems.forEach(item => {
       item.style.fontSize = '0px';
     });
-  } ,10000)
-
+  } ,11000)
   //On click logic
   let tracker= 0;
   let firstChance = true;
   let currentScore = 0;
+  const newParagraph = document.createElement('p');
   gridItems.forEach(item => {
     item.addEventListener("click", () => {
+      firstChance?  timerNote.innerHTML = "":firstChance = false;
       if(item.textContent-tracker == 1){
         item.style.backgroundColor= 'green';
         tracker++;
         currentScore += 10;
         item.style.fontSize = '20px';
+        timerNote.innerHTML = "Your current score";
+        timer.innerHTML = currentScore;
       }
       else{
         if(firstChance){
-          shakeButton.classList.add('shake');
-          setTimeout(() => {
-            shakeButton.classList.remove('shake');
-          }, 500);
           firstChance = false;
+          timerNote.innerHTML = "1st chance";
+          timer.innerHTML = currentScore;
+        timerNote.appendChild(newParagraph);
         }
         else{
         item.style.backgroundColor= 'red';
-        currentScore -=5;
+        currentScore -= 5;
         item.style.fontSize = '20px';
+        timerNote.innerHTML = "Your current score";
+        timer.innerHTML = currentScore;
         }
       }
     })
